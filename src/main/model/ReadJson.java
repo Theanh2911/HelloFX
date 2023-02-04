a214acb99dac081ff381790de842ceda216ffd43
Phan Cong Hieu <phanhieu2572002@gmail.com>
2023-02-04 18:33:06 +0700

    read diadiem from json

---
+++ src/main/model/ReadJson.java
@@ -0,0 +1,39 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ReadJson {
+    public static ArrayList<diadiem> listdiadiem = new ArrayList<diadiem>();
+    public static void main( String[] args){
+        JSONParser jsonP = new JSONParser();
+        try(FileReader reader = new FileReader("C:\\Users\\LENOVO\\IdeaProjects\\History_Project\\src\\diadiem.json")) {
+            Object obj = jsonP.parse(reader);
+            JSONArray empList = (JSONArray) obj;
+            empList.forEach(emp -> parseEmpObj((JSONObject)emp));
+          System.out.println(listdiadiem.get(1).nhanvat);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private static void parseEmpObj(JSONObject emp){
+        String ten = (String) emp.get("Title");
+        ArrayList<String> sukien = new ArrayList<String>();
+        sukien = (ArrayList<String>) emp.get("Historical_Events");
+        ArrayList<String> nhanvat = new ArrayList<String>();
+        nhanvat = (ArrayList<String>) emp.get("Historical_Characters");
+        listdiadiem.add(new diadiem(ten,sukien,nhanvat));
+
+    }
+}
---
+++ src/main/model/diadiem.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+import org.json.simple.JSONObject;
+public class diadiem {
+    public static int id;
+    public String ten;
+    public ArrayList<String> sukien = new ArrayList<String>();
+    public ArrayList<String> nhanvat = new ArrayList<String>();
+
+    public diadiem () {
+
+    }
+    public diadiem (String ten) {
+        this.ten = ten;
+    }
+
+    public diadiem (String ten, ArrayList<String> sukien)
+    {
+        this.ten = ten;
+        this.sukien = sukien;
+    }
+    public diadiem (String ten, ArrayList<String> sukien, ArrayList<String> nhanvat) {
+        this.ten = ten;
+        this.sukien = sukien;
+        this.nhanvat = nhanvat;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        diadiem.id = id;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public ArrayList<String> getSukien() {
+        return sukien;
+    }
+
+    public void setSukien(ArrayList<String> sukien) {
+        this.sukien = sukien;
+    }
+
+    public ArrayList<String> getNhanvat() {
+        return nhanvat;
+    }
+
+    public void setNhanvat(ArrayList<String> nhanvat) {
+        this.nhanvat = nhanvat;
+    }
+}
